2acbbf68e93df74740c9d860d218c3c0dfdcb0b7
Viiiiicky <vickytsai.0327@gmail.com>
2025-11-23 12:54:08 +0800

    更新JS及圖檔尺寸

--- assets/js/videoController.js
+++ assets/js/videoController.js
@@ -2,17 +2,50 @@ const webWorks = document.querySelectorAll(".web-works__link");
 const videos = document.querySelectorAll(".web-works__teaser");
 const thumbnails = document.querySelectorAll(".web-works__thumbnail");
 
-webWorks.forEach((link, index) => {
-  link.addEventListener("mouseenter", () => {
-    thumbnails[index].classList.add("hidden");
-    videos[index].classList.add("active");
-    videos[index].play();
-  });
+const mediaQuery = window.matchMedia("(min-width: 62.5em)");
 
-  link.addEventListener("mouseleave", () => {
-    videos[index].classList.remove("active");
-    thumbnails[index].classList.remove("hidden");
-    videos[index].pause();
-    videos[index].currentTime = 0;
+function handleMouseEnter(index) {
+  // 小螢幕時不執行
+  if (!mediaQuery.matches) return;
+
+  thumbnails[index].classList.add("hidden");
+  videos[index].classList.add("active");
+  videos[index].play();
+}
+
+function handleMouseLeave(index) {
+  // 小螢幕時不執行
+  if (!mediaQuery.matches) return;
+
+  videos[index].classList.remove("active");
+  thumbnails[index].classList.remove("hidden");
+  videos[index].pause();
+  videos[index].currentTime = 0;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  webWorks.forEach((link, index) => {
+    link.addEventListener("mouseenter", () => {
+      handleMouseEnter(index);
+    });
+
+    link.addEventListener("mouseleave", () => {
+      handleMouseLeave(index);
+    });
   });
 });
+
+// webWorks.forEach((link, index) => {
+//   link.addEventListener("mouseenter", () => {
+//     thumbnails[index].classList.add("hidden");
+//     videos[index].classList.add("active");
+//     videos[index].play();
+//   });
+
+//   link.addEventListener("mouseleave", () => {
+//     videos[index].classList.remove("active");
+//     thumbnails[index].classList.remove("hidden");
+//     videos[index].pause();
+//     videos[index].currentTime = 0;
+//   });
+// });
